a0f8afdc0ff2b5989c20b51ed9747a9b3b0faa54
lheuveline <l.heuveline@louisheuveline.com>
2023-12-28 03:22:38 +0100

    switched to transformers for whisper

--- src/asr/api.py
+++ src/asr/api.py
@@ -1,22 +1,29 @@
-import whisper
 from flask import Flask, request, send_file
 import os
-import torch
+import numpy as np
+import json
+
+from transformers import pipeline
 
 print("Starting API server...")
 app = Flask(__name__)
 print("Loading model...")
-model_name = os.environ.get("MODEL", "base")
-model = whisper.load_model(model_name)
-language = os.environ.get("LANGUAGE", "en")
+model = os.environ.get("MODEL", "base")
+device = os.environ.get("DEVICE")
+
+pipe = pipeline(
+    "automatic-speech-recognition", 
+    model=model, 
+    device=device
+)
 
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
     request_data = request.get_json()
-    audio_data = torch.Tensor(request_data["audio_data"])
-    result = model.transcribe(audio_data, language=language)
-    return result["text"]
+    audio_data = np.asarray(request_data["audio_data"])
+    result = pipe(audio_data, max_new_tokens=225)["text"]
+    return json.dumps(result)
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
---
+++ tests/test_asr.py
@@ -0,0 +1,30 @@
+import requests
+import torch
+import json
+
+import numpy as np
+
+from scipy.io.wavfile import read
+
+ASR_ENDPOINT="http://172.19.0.2:5000/transcribe"
+
+wav_filename = "tests/asr_test_wav.wav"
+sr, audio_data = read(wav_filename)
+audio_data = audio_data.astype(np.float32) / 32768.0
+audio_data = audio_data.tolist()
+
+headers = {
+    "Content-Type": "application/json"
+}
+query = {
+    "audio_data" : audio_data
+}
+
+r = requests.post(
+    ASR_ENDPOINT, data=json.dumps(query), headers=headers
+)
+print(r)
+if r.status_code != 200:
+    print(r.text)
+else: 
+    print(r.json())
